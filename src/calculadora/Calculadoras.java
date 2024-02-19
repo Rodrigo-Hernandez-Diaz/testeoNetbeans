16f3ee52f79b34fcacf747dda4583e3699c356e8
rodrigo <rherdia609@e.educaand.es>
2024-02-19 16:27:39 +0100

    calculo

---
+++ src/calculadora/Calculadoras.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Alumno Tarde
+ */
+public class Calculadoras {
+    
+    
+    
+    public double sumar(int x, int y){
+        return x+y;
+    }
+    
+    public double resta(int x, int y){
+        return x-y;
+    }
+}
